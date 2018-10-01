361572d72154643e0f139ba190c51b0a643e90fc
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-10-01 01:20:04 +0000

    Add more build steps; add publish script

--- build.sh
+++ build.sh
@@ -5,48 +5,19 @@ docker_version=''
 unifi_version=''
 declare -a docker_tags
 
-print_usage() {
-  echo "Usage: build.sh -v -u [-t]"
-}
-
-while getopts ":hv:u:t:" opt; do
-  case $opt in
-    v ) docker_version=$OPTARG
-      ;;
-    u ) unifi_version=$OPTARG
-      ;;
-    t ) docker_tags+=( "$OPTARG" )
-      ;;
-    h) print_usage
-      ;;
-    \? ) "Invalid Option: -$OPTARG" 1>&2
-      print_usage
-      exit 1
-      ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" 1>&2
-      print_usage
-      exit 1
-      ;;
-  esac
-done
-shift $((OPTIND -1))
-
-: "${docker_version:?-v required}"
-: "${unifi_version:?-u required}"
-
-# Parse unifi family from docker_version
-IFS='.' read -ra version_ary <<< "$docker_version"
-family="${version_ary[0]}.${version_ary[1]}"
-
-if [ ${#version_ary[@]} -le 2 ]; then
-  echo "Invalid version specified for -v"
-  exit 1
-fi
+. "lib/init_vars"
 
 # Source mo for templates
 . "lib/mo"
 
+echo 'Emitting Templates'
 mo < templates/manifest.yml > "${family}/manifest.yml"
 mo < templates/Dockerfile.amd64 > "${family}/Dockerfile.amd64"
 mo < templates/Dockerfile.arm32v7 > "${family}/Dockerfile.arm32v7"
+
+echo 'Building Images'
+docker build --pull -t ryansch/unifi-rpi:${docker_version}-amd64 -f ${family}/Dockerfile.amd64 ${family}
+docker build --pull -t ryansch/unifi-rpi:${docker_version}-arm32v7 -f ${family}/Dockerfile.arm32v7 ${family}
+
+echo 'Testing Images'
+
