c6dd8d894576a0659a796943f59961b735d3d613
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-05-12 09:10:04 -0300

    Rework multi platform builds

--- build.sh
+++ build.sh
@@ -21,9 +21,9 @@ mo < templates/entrypoint.sh > "${family}/entrypoint.sh"
 chmod +x "${family}/entrypoint.sh"
 
 echo 'Building Images'
-docker build --pull -t ryansch/unifi-rpi:${docker_version}-amd64 -f ${family}/Dockerfile.amd64 ${family}
-docker build --pull -t ryansch/unifi-rpi:${docker_version}-arm64v8 -f ${family}/Dockerfile.arm64v8 ${family}
+docker build --platform linux/amd64 --pull -t ryansch/unifi-rpi:${docker_version}-amd64 -f ${family}/Dockerfile.amd64 ${family}
+docker build --platform linux/arm64v8 --pull -t ryansch/unifi-rpi:${docker_version}-arm64v8 -f ${family}/Dockerfile.arm64v8 ${family}
 
 echo 'Testing Images'
-GOSS_PATH="${HOME}/.local/bin/goss-linux-amd64" GOSS_WAIT_OPTS='-r 60s -s 1s > /dev/null' dgoss run ryansch/unifi-rpi:${docker_version}-amd64
-GOSS_PATH="${HOME}/.local/bin/goss-linux-arm64" GOSS_WAIT_OPTS='-r 2000s -s 1s > /dev/null' dgoss run ryansch/unifi-rpi:${docker_version}-arm64v8
+GOSS_PATH="${HOME}/.local/bin/goss-linux-amd64" GOSS_WAIT_OPTS='-r 60s -s 1s > /dev/null' dgoss run --platform linux/amd64 ryansch/unifi-rpi:${docker_version}-amd64
+GOSS_PATH="${HOME}/.local/bin/goss-linux-arm64" GOSS_WAIT_OPTS='-r 60s -s 1s > /dev/null' dgoss run --platform linux/arm64v8 ryansch/unifi-rpi:${docker_version}-arm64v8
