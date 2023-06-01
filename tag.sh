5befe14814794021ab628c8bc737c5064ea1e89c
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-06-01 10:34:58 -0300

    Make tag work without pass

--- tag.sh
+++ tag.sh
@@ -12,9 +12,11 @@ declare -a docker_tags
 . "lib/mo"
 
 echo 'Emitting Manifest from Template'
-mo < templates/manifest.yml > "${family}/manifest.yml"
+mo <templates/manifest.yml >"${family}/manifest.yml"
 
 echo 'Pushing Manifest'
-username=$(basename $(ls ~/.password-store/docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/) .gpg)
-password=$(pass show docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/${username})
-manifest-tool --username=${username} --password=${password} push from-spec ${family}/manifest.yml
+# TODO: detect and use pass only if enabled
+# username=$(basename $(ls ~/.password-store/docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/) .gpg)
+# password=$(pass show docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/${username})
+# manifest-tool --username=${username} --password=${password} push from-spec ${family}/manifest.yml
+manifest-tool push from-spec "${family}/manifest.yml"
