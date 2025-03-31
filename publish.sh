e849f8a349c093c235715ae4c6f14cd275df662b
Ryan Schlesinger <ryan@ryanschlesinger.com>
2025-03-31 16:01:54 -0300

    Fix publish and tag

--- publish.sh
+++ publish.sh
@@ -19,6 +19,7 @@ docker_version=${docker_version} \
   java_package=${java_package} \
   docker buildx bake --file bake.hcl unifi --builder=container --push
 
+# shellcheck disable=SC2086
 docker_version=${docker_version} \
   unifi_version=${unifi_version} \
   unifi_sha256=${unifi_sha256} \
--- tag.sh
+++ tag.sh
@@ -6,9 +6,19 @@ set -euo pipefail
 # Source mo for templates
 . "lib/mo"
 
-echo 'Pushing Manifest'
-# TODO: detect and use pass only if enabled
-# username=$(basename $(ls ~/.password-store/docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/) .gpg)
-# password=$(pass show docker-credential-helpers/aHR0cHM6Ly9pbmRleC5kb2NrZXIuaW8vdjEv/${username})
-# manifest-tool --username=${username} --password=${password} push from-spec ${family}/manifest.yml
-manifest-tool push from-spec "${family}/manifest.yml"
+echo 'Pushing images/manifest'
+pushd "${family}"
+docker_version=${docker_version} \
+  unifi_version=${unifi_version} \
+  unifi_sha256=${unifi_sha256} \
+  java_package=${java_package} \
+  docker buildx bake --file bake.hcl unifi --builder=container --push
+
+# shellcheck disable=SC2086
+docker_version=${docker_version} \
+  unifi_version=${unifi_version} \
+  unifi_sha256=${unifi_sha256} \
+  java_package=${java_package} \
+  docker buildx bake --file bake.hcl unifi --builder=container --push \
+  ${docker_tags}
+popd
